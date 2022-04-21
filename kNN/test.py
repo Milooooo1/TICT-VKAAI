25588c5a4d8c62b9d3773c52307d18015fe45935
Milooooo1 <2002milo@gmail.com>
2022-04-21 16:17:24 +0200

    Updated bruteforce k script to work with verbose output

--- kNN/test.py
+++ kNN/test.py
@@ -1,9 +1,9 @@
 import os
 
 percentages = []
-for i in range(1, 366):
-    out = os.popen(f"python3.10 main.py {i}").read()
-    percentage = float(out[out.find(': ')+2:out.rfind("%")])
+for i in range(1, 100):
+    out = os.popen(f"python3.10 main.py -k {i}").read()
+    percentage = float(out[out.rfind('Accuracy: ')+10:out.rfind("%")])
     percentages.append(percentage)
     print(f"{percentage}% with k: {i}")
 
