f007c8ef27d24f66300291e558d37a66409e7337
Milo Beliën <milo.belien@student.hu.nl>
2022-05-16 17:34:55 +0200

    Documentation + variable input weights

--- NeuralNetworks/main.py
+++ NeuralNetworks/main.py
@@ -24,14 +24,31 @@ class Neuron:
             return 1 if sum >= self.bias else 0
 
     def __str__(self):
-        return f"Neuron {len(self.weights)} weights and Bias: {self.bias}"
+        '''Prints the number of weights and the bias'''
+        return f"Neuron Wn: {len(self.weights)} B: {self.bias}"
 
 class NeuralNetwork:
-    def __init__(self, num_layers, layer_depths):
+    def __init__(self, num_layers: int, layer_depths: list) -> None:
+        """_summary_
+
+        Args:
+            num_layers (int): The amount of layers that need to be generated
+            layer_depths (list): The depths of each of these layers
+
+        Raises:
+            IndexError: If not all the layers are matched with a depth an IndexError is raised 
+                        because it is not know how many neurons to generate for this layer
+        """
+        if num_layers != len(layer_depths):
+            raise IndexError("layer_depths need the same amount of layers as specified in num_layers")
+        
         self.matrix = [[] for n in range(0, num_layers)]
         for i in range(0, num_layers):
             for j in range(0, layer_depths[i]):
-                self.matrix[i].append(Neuron([random.uniform(-1, 1) for j in range(layer_depths[i])], random.uniform(-1, 1)))
+                if i == 0:
+                    self.matrix[i].append(Neuron([random.uniform(-1, 1) for j in range(layer_depths[i])], random.uniform(-1, 1)))
+                else: 
+                    self.matrix[i].append(Neuron([random.uniform(-1, 1) for j in range(layer_depths[i-1])], random.uniform(-1, 1)))
         
         [[print(neuron) for neuron in layer] and print() for layer in self.matrix]
 
@@ -63,7 +80,7 @@ def main():
     #         carry = NAND_gate.classify([out1, out1])
     #         print(f"{x1}, {x2}, {carry}, {sum}")
 
-    NeuralNetwork(3, [2, 2, 2])
+    NeuralNetwork(6, [2, 2, 3, 4, 2, 2])
 
 if __name__ == "__main__":
     main()
