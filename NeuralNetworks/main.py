e4f36c09113f9ee866498c99cd8dab0351fd7d6c
Milooooo1 <2002milo@gmail.com>
2022-05-23 17:42:34 +0200

    Deleted debugging print statements

--- NeuralNetworks/main.py
+++ NeuralNetworks/main.py
@@ -55,39 +55,28 @@ class NeuralNetwork:
             
             for neuron in layer:
                 acc = 0
-                # print(f"(1) zj = {round(neuron.bias, 2)} ", end="")
                 for depth, weight in enumerate(neuron.weights):
                     acc += weight * self.matrix[index-1][depth].output
-                    # print(f"+ ({round(weight, 2)} * {round(self.matrix[index-1][depth].output, 2)})", end=" ")
                 neuron.sum = neuron.bias + acc
                 neuron.output = sigmoid(neuron.sum)
-                # print(f"= {round(neuron.bias + acc, 2)} || aj = {round(neuron.output, 2)}")
             
-    def backPropagate(self, ground_truths: list):
+    def backpropagate(self, ground_truths: list):
         """calculate all the delta's for the whole network.
 
         Args:
             ground_truths (list): the known outputs that the network needs to match
         """
         # First calculate the output layer
-        # print(f"\nDelta = r'(zi) * (ground truth - output)")
         for neuron, ground_truth in zip(self.matrix[-1], ground_truths):
             neuron.delta = sigmoid_grad(neuron.sum) * (ground_truth - neuron.output)
-            # print(f"(2) D = {round(sigmoid_grad(neuron.sum), 2)} * {round(ground_truth, 2)} - {round(neuron.output, 2)} = {round(neuron.delta, 2)}")
         
         # Backpropagate the previous layers
-        # print(f"\nDelta = r'(zi) * (deltaj * Weightij + ...)")
         for index, layer in enumerate(reversed((self.matrix[1:-1]))):
             for depth, neuron in enumerate(layer):
                 acc = 0
-                # print(f"(3) D = {round(sigmoid_grad(neuron.sum), 2)} * ", end="")
                 for i, prevNeuron in enumerate(self.matrix[index-1]):
                     acc += prevNeuron.weights[depth] * prevNeuron.delta
-                    # print(f"({round(prevNeuron.weights[depth], 2)} * {round(prevNeuron.delta, 2)})", end=" ")
-                    # if i != len(self.matrix[index-1])-1:
-                        # print(f"+", end=" ")
                 neuron.delta = sigmoid_grad(neuron.sum) * acc
-                # print(f"= {round(sigmoid_grad(neuron.sum),2)} * {round(acc,2)} = {round(neuron.delta,2)}")
     
     def updateWeights(self, learning_rate: int):
         """Update the weights and biases of each neuron.
@@ -97,13 +86,10 @@ class NeuralNetwork:
                                  but can cause a less effective network. A lower learning rate is slower
                                  and results in a better network but can stop to early if its stuck.
         """
-        # print()
         for index, layer in enumerate(reversed(self.matrix[1:])):
             for neuron in layer:
                 for depth, weight in enumerate(neuron.weights):
                     tmp = len(self.matrix) - 1
-                    # print(f"(4) {round(weight,2)} += {learning_rate} * {round(neuron.delta, 2)} * {round(self.matrix[tmp-index-1][depth].output, 2)} = {round(neuron.weights[depth] + (learning_rate * neuron.delta * self.matrix[tmp-index-1][depth].output),2)}")     
-                    # print(f"(4) {round(neuron.bias, 2)} += {learning_rate} * {round(neuron.delta, 2)} = {round(neuron.bias + (learning_rate * neuron.delta), 2)}")     
                     neuron.weights[depth] += learning_rate * neuron.delta * self.matrix[tmp-index-1][depth].output
                     neuron.bias += learning_rate * neuron.delta
     
@@ -126,27 +112,20 @@ class NeuralNetwork:
             correct_ctr = 0
             loss = 0
             for index, data_point in enumerate(inputs):
-                # self.print()
                 
                 for input, neuron in zip(data_point, self.matrix[0]):
                     neuron.output = float(input)    
                     
                 self.feedForward()
-                self.backPropagate(outputs[index])
+                self.backpropagate(outputs[index])
                 self.updateWeights(lr)
                 
-                # print(outputs[index].index(max(outputs[index])))
                 network_outputs = [neuron.output for neuron in self.matrix[-1]]
-                # print(network_outputs.index(max(network_outputs)))
                 if outputs[index].index(max(outputs[index])) == network_outputs.index(max(network_outputs)):
                     correct_ctr += 1
                 for output, neuron in zip(outputs[index], self.matrix[-1]):
-                    # print(f"Neuron output: {neuron.output} vs ground truth: {output}")
                     loss += (output - neuron.output)
-                    # if (round(neuron.output) == output): 
-                        # correct_ctr+=1
-                    
-                # self.print()
+
             print(f"Epoch: {epoch}/{epochs} | {round(correct_ctr)}/{len(inputs)} correct | accuracy of: {round((correct_ctr) / len(inputs)*100)}% | loss: {round(loss/len(inputs), 8)} | [{round((epoch/epochs)*100,2)}%] completed      " , end="\r")
         print()
 
